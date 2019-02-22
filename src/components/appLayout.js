fb61398fe12e3da245c96429310060a52d9858a5
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-02-22 01:12:38 +0100

    First commit after install gatsby blog and ant-design

---
+++ src/components/appLayout.js
@@ -0,0 +1,135 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Layout, Menu, Icon } from "antd"
+import { rhythm, scale } from "../utils/typography"
+import "./layout.css"
+
+
+const {
+  Header, Footer, Sider, Content,
+} = Layout;
+
+class AppLayout extends React.Component {
+
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  render() {
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+    let header
+
+    if (location.pathname === rootPath) {
+      header = (
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            fontFamily: `Montserrat, sans-serif`,
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h3>
+      )
+    }
+
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: `1200px`,
+
+        }}
+      >
+        <Layout>
+          <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            trigger={null}
+            collapsible
+            collapsed={this.state.collapsed}
+            onBreakpoint={(broken) => { console.log(broken); }}
+            onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
+          >
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <Icon type="user" />
+                <span className="nav-text">nav 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="video-camera" />
+                <span className="nav-text">nav 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="upload" />
+                <span className="nav-text">nav 3</span>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <Icon type="user" />
+                <span className="nav-text">nav 4</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+
+          <Layout>
+            <Header><Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />{header}</Header>
+            <Content style={{ padding: `6rem 1rem 1rem 1rem` }}>{children}</Content>
+            <Footer>
+              © {new Date().getFullYear()}, Built with
+          {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </Footer>
+          </Layout>
+
+        </Layout>
+
+
+
+      </div>
+    )
+  }
+}
+
+export default AppLayout
---
+++ src/pages/index.js
@@ -0,0 +1,81 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import AppLayout from "../components/appLayout"
+import SEO from "../components/seo"
+import Img from 'gatsby-image'
+import { rhythm } from "../utils/typography"
+
+class IndexPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+      <AppLayout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="Latest posts"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <Img sizes={node.frontmatter.featuredImage.childImageSharp.sizes} />
+              <small>{node.frontmatter.date}</small>
+              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            </div>
+          )
+        })}
+      </AppLayout>
+    )
+  }
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 3
+      ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD MMMM, YYYY")
+            title
+            featuredImage {
+                childImageSharp{
+                    sizes(maxWidth: 630) {
+                        ...GatsbyImageSharpSizes
+                    }
+                }
+            }
+          }
+        }
+      }
+    }
+  }
+`
