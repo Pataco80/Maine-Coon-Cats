321020bbc14b1facdfb801d0476bbf1327aea951
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-02-25 14:17:30 +0100

    Création du fichier pour le header du site

--- src/components/appLayout.js
+++ src/components/appLayout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Layout, Menu, Icon } from "antd"
+import MainNavigation from "./mainNavigation"
+import { Layout, Icon } from "antd"
 import "../theme/index"
 import { rhythm, scale } from "../utils/typography"
 import "./layout.css"
@@ -90,24 +91,7 @@ class AppLayout extends React.Component {
             onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
           >
             <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1">
-                <Icon type="user" />
-                <span className="nav-text">nav 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="video-camera" />
-                <span className="nav-text">nav 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="upload" />
-                <span className="nav-text">nav 3</span>
-              </Menu.Item>
-              <Menu.Item key="4">
-                <Icon type="user" />
-                <span className="nav-text">nav 4</span>
-              </Menu.Item>
-            </Menu>
+            <MainNavigation />
           </Sider>
 
           <Layout>
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+
+
+<Icon
+  className="trigger"
+  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+  onClick={this.toggle}
+/>{ header }
