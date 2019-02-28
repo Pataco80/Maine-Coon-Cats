5e7b4e77574a72c585e37d2a2caa400106d0fdab
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-02-28 11:31:26 +0100

    Mise en page de la page de blog

--- src/components/mainNavigation.js
+++ src/components/mainNavigation.js
@@ -6,20 +6,16 @@ class MainNavigation extends React.Component {
     return (
       < Menu theme="light" mode="inline" defaultSelectedKeys={['1']} >
         <Menu.Item key="1">
-          <Icon type="user" />
-          <span className="nav-text">nav 1</span>
+          <Icon type="home" />
+          <span className="nav-text">Accueil</span>
         </Menu.Item>
         <Menu.Item key="2">
-          <Icon type="video-camera" />
-          <span className="nav-text">nav 2</span>
+          <Icon type="book" />
+          <span className="nav-text">Blog</span>
         </Menu.Item>
         <Menu.Item key="3">
-          <Icon type="upload" />
-          <span className="nav-text">nav 3</span>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="user" />
-          <span className="nav-text">nav 4</span>
+          <Icon type="mail" />
+          <span className="nav-text">Contact</span>
         </Menu.Item>
       </Menu >
     )
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
+import { Row, Col, Button } from "antd"
 import AppLayout from "../components/appLayout"
 import SEO from "../components/seo"
 import Img from 'gatsby-image'
@@ -21,21 +21,36 @@ class BlogIndex extends React.Component {
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <Img sizes={node.frontmatter.featuredImage.childImageSharp.sizes} />
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-              <p>Tags:{node.frontmatter.tags}</p>
-            </div>
+            <Row className="list-posts" type="flex" justify="center">
+              <Col className="posts-container" xs={20} md={22}>
+                <Row className="posts" type="flex">
+                  <div key={node.fields.slug}>
+                    <h3
+                      style={{
+                        marginBottom: rhythm(1 / 4),
+                      }}
+                    >
+                      <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                        {title}
+                      </Link>
+                    </h3>
+                    <small>{node.frontmatter.date}</small>
+                    <Row className="post-content" gutter={16} type="flex">
+                      <Col xs={3} md={6}>
+                        <Img sizes={node.frontmatter.featuredImage.childImageSharp.sizes} />
+                      </Col>
+                      <Col xs={21} md={18} className="post-info">
+                        <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                        <Button><Link style={{ boxShadow: 'none' }} to={node.fields.slug}>En savoir plus</Link></Button>
+                        <p>Tags:{node.frontmatter.tags}</p>
+                      </Col>
+                    </Row>
+                  </div>
+                </Row>
+
+              </Col>
+            </Row>
+
           )
         })}
       </AppLayout>
