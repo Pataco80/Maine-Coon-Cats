0ab4c1e3e934858c8f181202f0e5d9b176c9be2c
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-03-01 11:38:39 +0100

    suppression suite au changement de nom de fichier

--- src/theme/index.js
+++
@@ -1,8 +0,0 @@
-
-// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
-module.exports = {
-  'body-background': '#fff',
-  'link-color': '#00f',
-  'border-radius-base': '2px',
-  'card-actions-background': '#f5f8fa'
-};
